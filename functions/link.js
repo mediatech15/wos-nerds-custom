632aab928f898288c4185f33e50d10df64dd040f
Kyle Mason <kyle.e.mason@outlook.com>
2026-03-24 14:10:50 -0400

    update

--- functions/link.js
+++ functions/link.js
@@ -2,5 +2,9 @@ export function onRequestPost (context) {
   return new Response(JSON.stringify({
     context: context,
     msg: "Yo"
-  }))
+  }), {
+    status: 200,
+    statusText: 'OK',
+    headers: new Headers().append('Content-Type', 'application/json')
+  })
 }
