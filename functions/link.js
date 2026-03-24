b2152eafd68c103ace2d48deb6a0754d5d7cb5e2
Kyle Mason <kyle.e.mason@outlook.com>
2026-03-24 15:40:50 -0400

    update

--- functions/link.js
+++ functions/link.js
@@ -1,18 +1,55 @@
 export async function onRequestPost (context) {
-  const x = await context.request.json()
-  const h = {}
+  let body
+  try{
+    body = await context.request.json()
+  } catch {
+    return new Response('Error getting body', {status: 400})
+  }
+  const headers = {}
   for (const k of context.request.headers.keys()){
     h[k] = context.request.headers.get(k)
   }
-  return Response.json({
-    context: context,
-    url: context.request.url,
-    cf: context.request.cf,
-    request: x,
-    headers: h,
-    msg: "Yo"
-  }, {
-    status: 200,
-    statusText: 'OK',
+  if (headers.origin !== 'https://wos-tools.fidgetcode.dev') {
+    return new Response('Forbidden', {status: 403})
+  }
+  if (headers.referer !== 'https://wos-tools.fidgetcode.dev/') {
+    return new Response('Forbidden', {status: 403})
+  }
+  if (headers['sec-fetch-site'] !== 'same-origin') {
+    return new Response('Forbidden', {status: 403})
+  }
+
+  if (body.url === undefined) {
+    return new Response('url must be defined in body', {status: 400})
+  }
+  if (body.id === undefined) {
+    return new Response('id must be defined in body', {status: 400})
+  }
+
+  const controller = new AbortController()
+  const timeout = setTimeout(() => controller.abort(), 10000)
+  const reqBody = new URLSearchParams({
+    action: 'save_by_keyword',
+    keyword: body.id,
+    url: body.url,
+    format: 'json',
+    signature: '24b1328eb0'
+  }).toString()
+  const link = await fetch('https://micro.pink/yourls-api.php', {
+    method: 'POST',
+    body: reqBody,
+    signal: controller.signal
   })
+  clearTimeout(timeout)
+
+  if (link.ok) {
+    const linkData = await link.json()
+    const jsonData = JSON.parse(linkData)
+    return Response.json({
+      link: jsonData.shorturl,
+      message: jsonData.message
+    })
+  }
+
+  return new Response('error creating short link', {status: 400})
 }
