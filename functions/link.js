f1b39d9d3a2aa64522b687c12f2c0dad7a0197d5
Kyle Mason <kyle.e.mason@outlook.com>
2026-03-24 14:37:49 -0400

    update

--- functions/link.js
+++ functions/link.js
@@ -2,6 +2,8 @@ export async function onRequestPost (context) {
   const x = await context.request.json()
   return Response.json({
     context: context,
+    url: context.request.url,
+    headers: context.request.headers.entries(),
     request: x,
     msg: "Yo"
   }, {
